4321e482c9dda315b59b520c1e631e1ae7cc6e87
CCandruss <ccandruss@gmail.com>
2022-06-23 22:23:38 -0500

    test

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -5,7 +5,7 @@ import { ContactList } from './ContactList';
 //contact name,email, image
 export const App = () => (
   <div>
-    <h1>Meteor Wallet</h1>
+    <h1>Meteor Wallet!</h1>
     <ContactForm/>
     <ContactList/>
   </div>
